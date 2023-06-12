c11e957455206ddc45ef393150cfd5118a5a5621
zhaoyuan <909719666@qq.com>
2023-06-12 14:37:15 +0800

    日期工具类增加方法

--- src/utils/DateUtil.php
+++ src/utils/DateUtil.php
@@ -92,5 +92,94 @@ class DateUtil
         return strtotime('today') - ($days * 86400);
     }
 
-    
+    /**
+     * 获取指定月份开始和结束时间戳
+     *
+     * @param integer $year
+     * @param integer $month
+     * @return array
+     */
+    public function getMonthTimestamps(int $year, int $month)
+    {
+        $start = strtotime("{$year}-{$month}-01 00:00:00");
+        $end   = strtotime(date("Y-m-t 23:59:59", $start));
+        return [$start, $end];
+    }
+
+    /**
+     * 获取指定年份开始和结束时间戳
+     *
+     * @param integer $year
+     * @return array
+     */
+    public function getYearTimestamps(int $year)
+    {
+        $start = strtotime("{$year}-01-01 00:00:00");
+        $year += 1;
+        $end = strtotime("{$year}-01-01 00:00:00") - 1;
+        return [$start, $end];
+    }
+
+    /**
+     * 获取指定季度的开始和结束时间戳
+     *
+     * @param integer $year
+     * @param integer $type
+     * @return array
+     */
+    public function getQuarterTimestamps(int $year, int $type)
+    {
+        switch ($type) {
+            case 1:
+                return [strtotime("{$year}-01-01 00:00:00"), strtotime("{$year}-04-01 00:00:00") - 1];
+                break;
+            case 2:
+                return [strtotime("{$year}-04-01 00:00:00"), strtotime("{$year}-07-01 00:00:00") - 1];
+                break;
+            case 3:
+                return [strtotime("{$year}-07-01 00:00:00"), strtotime("{$year}-10-01 00:00:00") - 1];
+                break;
+            case 4:
+                $next_year = $year + 1;
+                return [strtotime("{$year}-10-01 00:00:00"), strtotime("{$next_year}-01-01 00:00:00") - 1];
+                break;
+            default:
+                return [0, 0];
+                break;
+        }
+    }
+
+    /**
+     * 获取指定月份指定周的开始和结束时间戳
+     *
+     * @param integer $year
+     * @param integer $month
+     * @param integer $week
+     * @return array
+     */
+    public function getWeekTimestamps(int $year, int $month, int $week)
+    {
+        $month_start_time = strtotime("{$year}-{$month}-01");
+        $month_end_time   = strtotime(date('Y-m-t 23:59:59', $month_start_time));
+
+        // 1号是周几
+        $startWeek = date('N', $month_start_time);
+
+        //第一周结束时间
+        $oneWeekEndTime = $month_start_time + 86400 * (8 - $startWeek);
+
+        $diff_time = ($week - 1) * 604800;
+
+        $start_time = $oneWeekEndTime - 604800 + $diff_time;
+        $end_time   = $oneWeekEndTime - 1 + $diff_time;
+
+        if ($start_time > $month_end_time || $end_time < $month_start_time) {
+            return [0, 0];
+        }
+
+        $start_time = $start_time < $month_start_time ? $month_start_time : $start_time;
+        $end_time   = $end_time > $month_end_time ? $month_end_time : $end_time;
+
+        return [$start_time, $end_time];
+    }
 }
